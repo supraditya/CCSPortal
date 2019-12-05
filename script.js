e6a6ccb1345f730d0b86e64ac000c7e61001124f
[supraditya] <[aditya300100@gmail.com]>
2019-12-05 09:35:01 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+$(".info-item .btn").click(function(){
+  $(".container").toggleClass("log-in");
+});
+$(".container-form .btn").click(function(){
+  $(".container").addClass("active");
+});
+$(".container-form .btn").click(function(){
+  	$(".table-cell").css("background-color","#FAA41A");
+});
